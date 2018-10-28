09418123db20011547b2e04d8e89883d5fa7d82c
huangyin0611 <1071324110@qq.com>
2018-10-28 17:54:01 +0800

    cost function

--- NN/Compute_Cost.py
+++ NN/Compute_Cost.py
@@ -3,6 +3,25 @@
 # @Author   : HuangYin
 # @FileName : Compute_Cost.py
 # @Software : PyCharm
+import numpy as np
 
-def compute_Cost(A2,Y,parameter):
 
+def compute_Cost(A2, Y, parameter):
+    """
+       Computes the cross-entropy cost given in equation (13)
+
+       Arguments:
+       A2 -- The sigmoid output of the second activation, of shape (1, number of examples)
+       Y -- "true" labels vector of shape (1, number of examples)
+       parameters -- python dictionary containing your parameters W1, b1, W2 and b2
+
+       Returns:
+       cost -- cross-entropy cost given equation (13)
+    """
+    m = Y.shape[1]
+    logprobs = np.multiply(np.log(A2), Y) + np.multiply(np.log(1 - A2), 1 - Y)
+    cost = -np.sum(logprobs) / m
+
+    assert (isinstance(cost, float))
+
+    return cost
